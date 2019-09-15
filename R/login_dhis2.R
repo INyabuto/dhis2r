e317322836d19ea762df7dbf8052a882cdf6cf81
INyabuto <isaiahnyabuto@gmail.com>
2019-09-15 21:46:15 +0300

    Updated the package name Added a new function to extract secret from windows and updated the function documentations

--- R/login_dhis2.R
+++ R/login_dhis2.R
@@ -1,6 +1,6 @@
 #'Login to a DHIS2 instance
 #'
-#'@param host A server domain
+#'@param host A string. Server domain name
 #'@return Login details
 #'@examples
 #'base <- "clone.psi-mis.org"
@@ -42,3 +42,27 @@ print.login_dhis2 <- function(x, ...){
   invisible(x)
 }
 
+
+#'Retrieve secret from windows
+#'
+#'\code{win_secret} returns an invisible decrypted password stored with
+#'Micrososft data protection API.
+#'
+#'Invisible means that the password won't be displayed in the console, but can
+#'be assigned to a variable or used inline.'Under the hood, \code{win_secret} is
+#'using \code{\link[keyringr]decrypt_dpapi_pw} to help retrieve and unlock
+#'passwords stored with the Microsoft data protection API. It Requires
+#'Powershell to be installed and execution policy set to RemoteSigned.
+#'@seealso \code{\link[keyringr]{decrypt_dpapi_pw}}
+#'@param cred_label A string. Usually the name of your secret file.
+#'@return An invisible string.
+#'
+win_secret <- function(cred_label){
+
+  credential_path <- paste(Sys.getenv("USERPROFILE"),
+                           '\\DPAPI\\passwords\\',
+                           Sys.info()["nodename"],
+                           '\\', credential_label, '.txt', sep="")
+  decrypt_dpapi_pw(credential_path)
+
+}
