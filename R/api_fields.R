b7b340791036d6d2a8cd75a18d604cd61cca0b5d
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-02-13 14:11:06 +0300

    define api fields

---
+++ R/api_fields.R
@@ -0,0 +1,44 @@
+#' Parse DHIS2 API fields
+#'
+#' This function parse API fields params in standard DHIS2 format.
+#'
+#' @export
+#' @examples
+#' # select all the fields
+#' api_fields("*")
+#' # select only name, id, shortName and code
+#' api_fields("name","id","shortName","code")
+#' # select fields in collection
+#' api_fields("name", organisationUnits = c("name","id","code"))
+api_fields <- function(...){
+
+  args <- list(...)
+
+  arg_names <- names(args)
+
+  missing <-
+    if (is.null(arg_names)){
+      rep(TRUE, length(args))
+    } else {
+      !nzchar(arg_names)
+    }
+
+  collections <- args[!missing]
+
+  collection_fields <- purrr::map(collections, commas)
+
+  collection_names <- names(collection_fields)
+
+  collection_transformed <- vector("list", length(collection_fields))
+
+  for (i in seq_along(collection_names)){
+    collection_transformed[[i]] <- paste0(collection_names[i], "[", collection_fields[[i]], "]")
+  }
+
+  paste(
+    paste0(args[missing], collapse = ","),
+    paste0(collection_transformed, collapse = ","),
+    sep = ","
+  )
+
+}
