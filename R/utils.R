9c4a9d612df2835928864bd9281bcf98156e9a7b
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-11-23 16:48:53 +0300

    cleaning internals

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,18 @@
-api_version <- function(version = 29) if (version %in% 30:28) version else stop("unsupported API version", call. = F)
-
-#' @export
-api_order <- function(x, order_by = c("asc", "iasc", "desc", "idesc")){
-  paste0(x, ":", match.arg(order_by))
+#' API version
+#'
+#' Sets an API version. Last three versions of DHIS2 are supported.
+#'
+#' @param version An integer. The API version.
+#' @return Integer
+#' @keywords internal
+api_version <- function(version = 29) {
+  if (as.integer(version) %in% 30:28)
+    version
+  else
+    stop("unsupported API version", call. = F)
 }
+
+
 
 
 #' %>%
@@ -16,20 +25,20 @@ api_order <- function(x, order_by = c("asc", "iasc", "desc", "idesc")){
 NULL
 
 
-# empty list
-is.empty <- function(...){
-  args <- list(...)
-  identical(length(args), 0L)
-}
+# # empty list
+# is.empty <- function(...){
+#   args <- list(...)
+#   identical(length(args), 0L)
+# }
+#
+# # named list
+# has_names <- function(...){
+#   args <- list(...)
+#   names <- names(args)
+#   !is.null(names)
+# }
 
-# named list
-has_names <- function(...){
-  args <- list(...)
-  names <- names(args)
-  !is.null(names)
-}
 
 commas <- function(...) paste0(..., collapse = ",")
 
-#'@export
-api_filter <- function(x, filter, property) paste0(x, ":", filter, ":", property)
+
