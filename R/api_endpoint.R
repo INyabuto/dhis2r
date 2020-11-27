a2c3a5ad0e1d8843521fabbc45bb4ef5536f444a
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-11-27 12:11:57 +0300

    add ref and ref_id to the API, & some bit of cleaning

--- R/api_endpoint.R
+++ R/api_endpoint.R
@@ -1,18 +1,16 @@
-#' Generate a DHIS2 API endpoint link
+#' Modify a DHIS2 API endpoint link
 #'
-#' Short hand to generate a DHIS2 like API endpoint link from R. This is useful
-#' if you need to define or parse API parameters quickly. By default, the
-#' resources endpoint is returned.
+#' Short hand to modify a `DHIS2 like` API endpoint link from R. This is useful
+#' if you need to define or parse API parameters quickly.
 #'
 #'
-#' @name generate_api_endpoint
+#' @name modify_api_endpoint
 #'
-#' @param ... Additional parameters parsed to \code{\link{api_query}}.
-#' @param resource A character string, the DHIS2 resource name, example
-#'   `dataElements`, `dataElementGroups`, e.tc.
-#' @param id A character string, the unique identifier for a specific object in
-#'   the resource.
-#' @param path A character string, specifies a sub directory in the resource.
+#' @param endpoint A DHIS2 endpoint, example `dataElements`, `dataElementGroups`, e.tc.
+#' @param ... Query parameters parsed to \code{\link{api_query}}.
+#' @param id The unique identifier for an object in the endpoint.
+#' @param ref A reference within the endpoint.
+#' @param ref_id The unique identifier of a referenced object in the endpoint.
 #' @param version An integer, the version of DHIS2 web API. The default version
 #'   is `current supported version`, you can override this using the
 #'   \code{\link{api_version}} or by specifying the value explicitly.
@@ -20,18 +18,19 @@
 #' @export
 #' @examples
 #' # default api link
-#' generate_api_endpoint()
+#' modify_api_endpoint()
 #' # navigate to dataElements endpoint
-#' generate_api_endpoint(resource = "dataElements")
+#' modify_api_endpoint(endpoint = "dataElements")
 #' # select data elements name, id and shortNames
-#' generate_api_endpoint(resource = "dataElements", fields = c("name", "id", "shortNames"))
+#' modify_api_endpoint(endpoint = "dataElements", fields = c("name", "id", "shortNames"))
 #' # arrange the data elements name in ascending order
-#' generate_api_endpoint(
-#'   resource = "dataElements", fields = c("name", "id", "shortNames"),
+#' modify_api_endpoint(
+#'   endpoint = "dataElements", fields = c("name", "id", "shortNames"),
 #'   order = api_order("name", by = "asc")
 #' )
-generate_api_endpoint <- function(..., resource = "resources", id = NULL, path = NULL, version = NULL) {
-  params <- parse_api_params(..., resource = resource, id = id, path = path, version = version)
+modify_api_endpoint <- function(endpoint, ..., id = NULL, ref = NULL, ref_id = NULL,
+                                version = NULL) {
+  params <- parse_api_params(endpoint, ..., id = id, ref = ref, ref_id = ref_id, version = version)
 
   build_api_endpoint(params)
 }
@@ -41,22 +40,24 @@ generate_api_endpoint <- function(..., resource = "resources", id = NULL, path =
 #'
 #' This function parse DHIS2 API endpoint parameters into a list object.
 #'
-#' @inheritParams generate_api_endpoint
+#' @inheritParams modify_api_endpoint
 #'
 #' @return An S3 object of class `api_params`.
 #' @export
 #' @examples
-#' # default resource endpoint
+#' # default endpoint endpoint
 #' api_params <- parse_api_params()
 #' # api params with query object
-#' api_params <- parse_api_params(resource = "dataElements", field = c("name", "code", "id"))
-parse_api_params <- function(..., resource = NULL, id = NULL, path = NULL, version = NULL) {
+#' api_params <- parse_api_params(endpoint = "dataElements", field = c("name", "code", "id"))
+parse_api_params <- function(endpoint, ..., id = NULL, ref = NULL, ref_id = NULL,
+                             version = NULL) {
   # parse the parameters to an S3 type object
   structure(
     list(
-      endpoint = resource,
+      endpoint = endpoint,
       id = id,
-      path = path,
+      ref = ref,
+      ref_id = ref_id,
       version = version,
       query = api_query(...)
     ),
@@ -79,21 +80,28 @@ build_api_endpoint <- function(params) {
   if (!is.null(params$endpoint)) {
     endpoint <- params$endpoint
   } else {
-    endpoint <- "resources"
+    endpoint <- "endpoints"
   }
 
-  # parse resource id
+  # parse endpoint id
   if (!is.null(params$id)) {
     id <- params$id
   } else {
     id <- 0
   }
 
-  # parse api path
-  if (!is.null(params$path)) {
-    path <- params$path
+  # parse api sub endpoint
+  if (!is.null(params$ref)) {
+    ref <- params$ref
   } else {
-    path <- 0
+    ref <- 0
+  }
+
+  # parse api sub endpoint id
+  if (!is.null(params$ref_id)) {
+    ref_id <- params$ref_id
+  } else {
+    ref_id <- 0
   }
 
   # parse the api version
@@ -111,7 +119,7 @@ build_api_endpoint <- function(params) {
   }
 
   paste(
-    paste("api", version, endpoint, id, path, sep = "/"),
+    paste("api", version, endpoint, id, ref, ref_id, sep = "/"),
     query,
     sep = "/"
   ) -> link
