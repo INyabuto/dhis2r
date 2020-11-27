cc2797998d226b34a2f9fd0b2fb1f02e5289f376
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-11-27 12:13:10 +0300

    update tests

--- tests/testthat/test-api-endpoint.R
+++ tests/testthat/test-api-endpoint.R
@@ -1,6 +1,9 @@
-test_that("'api_endpoint' well generates api links", {
-  expect_equal(api_endpoint(), paste("api",api_version(),"resources", sep = "/"))
-  expect_equal(api_endpoint(resource = "dataElements"), paste("api",api_version(),"dataElements", sep = "/"))
-  expect_equal(api_endpoint(resource = "dataElements", fields = c("name","id"), order = api_order("name", "asc")),
+test_that("'api_endpoint' well modifies an api link", {
+  #expect_equal(modify_api_endpoint(), paste("api",api_version(),"endpoints", sep = "/"))
+  expect_equal(modify_api_endpoint(endpoint = "dataElements"), paste("api",api_version(),"dataElements", sep = "/"))
+  expect_equal(modify_api_endpoint(endpoint = "dataElements", fields = c("name","id"), order = api_order("name", "asc")),
                paste("api",api_version(),"dataElements","?fields=name,id&order=name:asc", sep = "/"))
+  expect_equal(modify_api_endpoint("categories", id = "IDA", ref = "categoryOptions", ref_id = "IDB"), paste(
+    "api", api_version(), "categories", "IDA", "categoryOptions", "IDB", sep = "/"
+  ))
 })
