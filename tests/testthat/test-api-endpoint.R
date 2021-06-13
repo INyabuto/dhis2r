083d6c6b080950c867fcc4627b30b7aa5e837fd1
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2021-06-13 17:29:23 +0300

    update tests

--- tests/testthat/test-api-endpoint.R
+++
@@ -1,9 +0,0 @@
-test_that("'api_endpoint' well modifies an api link", {
-  #expect_equal(modify_api_endpoint(), paste("api",api_version(),"endpoints", sep = "/"))
-  expect_equal(modify_api_endpoint(endpoint = "dataElements"), paste("api",api_version(),"dataElements", sep = "/"))
-  expect_equal(modify_api_endpoint(endpoint = "dataElements", fields = c("name","id"), order = api_order("name", "asc")),
-               paste("api",api_version(),"dataElements","?fields=name,id&order=name:asc", sep = "/"))
-  expect_equal(modify_api_endpoint("categories", id = "IDA", ref = "categoryOptions", ref_id = "IDB"), paste(
-    "api", api_version(), "categories", "IDA", "categoryOptions", "IDB", sep = "/"
-  ))
-})
--- tests/testthat/test-modify-api-endpoint.R
+++
@@ -1,9 +0,0 @@
-test_that("'modify_api_endpoint' well modifies an api link", {
-  #expect_equal(modify_api_endpoint(), paste("api",api_version(),"endpoints", sep = "/"))
-  expect_equal(modify_api_endpoint(endpoint = "dataElements"), paste("api","dataElements", sep = "/"))
-  expect_equal(modify_api_endpoint(endpoint = "dataElements", fields = c("name","id"), order = api_order("name", "asc")),
-               paste("api","dataElements","?fields=name,id&order=name:asc", sep = "/"))
-  expect_equal(modify_api_endpoint("categories", id = "IDA", ref = "categoryOptions", ref_id = "IDB"), paste(
-    "api", "categories", "IDA", "categoryOptions", "IDB", sep = "/"
-  ))
-})
